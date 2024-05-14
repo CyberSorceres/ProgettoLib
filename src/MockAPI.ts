6fd85b877d2647b90d21d9ee5b12d3064c0bd418
giuliadentone <giuliaadentone@gmail.com>
2024-05-14 11:56:58 +0200

    messo json nel mock e aggiunto campi mancanti in user story

--- src/MockAPI.ts
+++ src/MockAPI.ts
@@ -1,11 +1,14 @@
 import { API } from "./API";
 
 export class MockAPI implements API{
-    //code
-
+    
     public getProgetto(id: string): Promise<any>{
-        //costruisco il json per un progetto
-        //return ; //ritorna il json
+        const progetto = {
+        id: id,
+        nome: `Progetto ${id}`,
+        validato: Math.random() > 0.5 // Simula una validazione casuale tra true 1 e false 0
+        };
+        
+        return Promise.resolve(progetto);
     }
-    */
 }
--- src/UserStory.ts
+++ src/UserStory.ts
@@ -1,7 +1,16 @@
+enum State {
+    TO_DO,
+    IN_PROGRESS,
+    DONE
+}
+
 export class UserStory{
+    
     private _id : string; 
     private _tag : string;
     private _description : string;
+    private _state : State;
+    private _verified: boolean;
 
     constructor (id: string){
         this._id = id; 
@@ -16,6 +25,14 @@ export class UserStory{
     public get tag(): string{
         return this._tag; 
     }
+
+    public get verified(): boolean{
+        return this._verified;
+    }
+
+    public get state(): State{
+        return this._state;
+    };
 
     public set tag(theTag: string){
         this._tag = theTag;
