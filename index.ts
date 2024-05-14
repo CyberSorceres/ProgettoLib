ae8767d16369895b4e25da62361ef8a26e2b6308
uiob <uiob1210@gmail.com>
2024-05-14 09:34:06 +0200

    first code structure

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log("HelloWorld");
+import { Progetto } from "./src/Progetto";
+import { EpicStory } from "./src/EpicStory";
+import { UserStory } from "./src/UserStory";
+import { Test } from "./src/Test";
+
+export {Progetto, EpicStory, UserStory, Test};
