bfb221898af762e35888f074a9c0153b4527f5e0
uiob <uiob1210@gmail.com>
2024-05-15 09:54:34 +0200

    fix test

--- tests/unit/EpicStory.test.ts
+++ tests/unit/EpicStory.test.ts
@@ -4,29 +4,31 @@ import { MockAPI } from '../../src/MockAPI';
 
 describe('EpicStory', () => {
 
+    let story: EpicStory;
+
+    beforeEach(() => {
+        story = new EpicStory('1');
+    });
+
     test('should set the id correctly through the constructor and should return the correct id using the getter', () => {
-        const id = '123';
-        const story = new EpicStory(id);
-        expect(story.id).toBe(id);
+        expect(story.id).toBe('1');
     })
 
     test('should return undefined for descrizione if not set', () => {
-        const story = new EpicStory('123');
         expect(story.descrizione).toBeUndefined();
     })
 
     test('should set any descrizione correctly', () => {
-        const story = new EpicStory('123');
         const descrizione = 'This is a new Epic Story';
         story.descrizione = descrizione;
         expect(story.descrizione).toBe(descrizione);
     })
 
     test('should correctly fetch fata for a new EpicStory', async () => {
-        let story = new EpicStory('1');
         await story.fetchData(new MockAPI());
 
         expect(story.id).toBe('1');
         expect(story.descrizione).toBe("Epic Story 1");
     })
+    
 });
---
+++ tests/unit/Test.test.ts
@@ -0,0 +1,9 @@
+import { describe, test, expect } from "vitest";
+
+describe('Test', () => {
+
+    test('should set the id correctly through the constructor and should return the correct id using the getter', () => {
+        expect(true).toBe(true);
+    })
+
+})
