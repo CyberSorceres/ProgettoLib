6cb001ef3db12d2706e7abdce5c3b3f537a307fe
giuliadentone <giuliaadentone@gmail.com>
2024-05-16 14:52:52 +0200

    epicstory

--- src/EpicStory.ts
+++ src/EpicStory.ts
@@ -10,7 +10,7 @@ export interface EpicData {
 export class EpicStory{
 	private _id: string;
 	private _descrizione: string;
-	private userStories: UserStory[] = [];
+	private _userStories: UserStory[] = [];
 	
 	
 	constructor(id: string) {
@@ -23,12 +23,11 @@ export class EpicStory{
 			const epicData = await myAPI.getEpicStory(this.id);
 			
 			if (epicData) {
-				this.descrizione = epicData.descrizione;
+				this._descrizione = epicData.descrizione;
 				for (const userStoryId of epicData.userStories) {
 					let user = new UserStory(userStoryId);
 					user.fetchData(myAPI);
-					this.userStories.push(user);
-	
+					this._userStories.push(user);
 					}
 			} else {
 				// Handle case where project data is not found
@@ -39,6 +38,8 @@ export class EpicStory{
 			throw new Error(`Error fetching epic story data for ID ${this.id}:`, error);
 		}
 	}
+	
+
 	public get id():string {
 		return this._id;
 	}
@@ -51,8 +52,8 @@ export class EpicStory{
 		this._descrizione = descrizione;
 	}
 
-	public getUserStories(): UserStory[] {
-        return this.userStories;
+	public get userStories(): UserStory[] {
+        return this._userStories;
     }
 
     public getUserStoryById(id: string): UserStory | undefined {
--- tests/unit/EpicStory.test.ts
+++ tests/unit/EpicStory.test.ts
@@ -24,13 +24,34 @@ describe('EpicStory', () => {
         expect(story.descrizione).toBe(descrizione);
     })
 
-    test('should correctly fetch fata for a new EpicStory', async () => {
+    test('should correctly fetch data for a new EpicStory', async () => {
         await story.fetchData(new MockAPI());
-
         expect(story.id).toBe('1');
         expect(story.descrizione).toBe("Epic Story 1");
     })
 
+    test('should return the correct UserStory by ID', async () => {
+        await story.fetchData(new MockAPI());
+        const expectedUserStory = story.userStories[0];
+        const userStoryById = story.getUserStoryById(expectedUserStory.id);
+        expect(userStoryById).toEqual(expectedUserStory);
+    })
     
+    test('should return undefined if UserStory ID is not found', () => {
+        const userStoryById = story.getUserStoryById('nonexistent_id');
+        expect(userStoryById).toBeUndefined();
+    })
+
+    test('should return the correct UserStory by tag', async () => {
+        await story.fetchData(new MockAPI());
+        const expectedUserStory = story.userStories[0];
+        const userStoryByTag = story.getUserStoryByTag(expectedUserStory.tag);
+        expect(userStoryByTag).toEqual(expectedUserStory);
+    });
+    
+    test('should return undefined if UserStory tag is not found', () => {
+        const userStoryByTag = story.getUserStoryByTag('nonexistent_tag');
+        expect(userStoryByTag).toBeUndefined();
+    });
     
 });
