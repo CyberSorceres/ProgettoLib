7f53d6b9a5e3210c6d707a8c72281eb69431e00c
uiob <uiob1210@gmail.com>
2024-05-23 16:13:01 +0200

    added feedback and import/exports in index

--- index.ts
+++ index.ts
@@ -6,5 +6,7 @@ import { API } from "./src/API";
 import { API_interface } from "./src/API_interface";
 import { MockAPI } from "./src/MockAPI";
 import { mockUserStories, mockEpicStories, mockProjects, ProjectData, EpicData, UserData } from "./src/MockData";
+import { RUOLO, User } from "./src/User";
+import { Feedback } from "./src/Feedback";
 
-export {Progetto, EpicStory, UserStory, Test, API, API_interface, MockAPI, UserData, EpicData, ProjectData, mockUserStories, mockEpicStories, mockProjects};
+export {Progetto, EpicStory, UserStory, Test, User, RUOLO, Feedback, API, API_interface, MockAPI, UserData, EpicData, ProjectData, mockUserStories, mockEpicStories, mockProjects};
