fc1bc702a6678fa0c4be5030ec1dc06428683d7d
uiob <uiob1210@gmail.com>
2024-05-23 14:56:57 +0200

    updated progetto's tests

--- tests/unit/Progetto.test.ts
+++ tests/unit/Progetto.test.ts
@@ -1,34 +1,34 @@
 import { describe, expect, beforeEach, test} from 'vitest';
 import { Progetto } from '../../src/Progetto';
 import { MockAPI } from '../../src/MockAPI';
-import { mockEpicStories } from '../../src/MockData';
-import { EpicStory } from '../../src/EpicStory';
-
+import { API_interface } from '../../src/API_interface';
 
 describe('Progetto', () => {
-    let progetto: Progetto;
+    let initializedProgetto: Progetto;
+    let nonInitializedProgetto: Progetto;
+    let api: API_interface;
 
-    beforeEach(() => {
-        progetto = new Progetto('1');
+    beforeEach(async () => {
+        api = new MockAPI;
+        const pro = await api.getProgetto('1');
+        if(pro != null){
+            initializedProgetto = pro;
+        }
+        nonInitializedProgetto = new Progetto();
     });
 
-    test('should initialize with given id', () => {
-        expect(progetto.id).toBe('1');
+    test('should initialize with undefined fields', () => {
+        expect(nonInitializedProgetto.name).toBeUndefined();
+        expect(nonInitializedProgetto.isValidated).toBeUndefined();
+        expect(nonInitializedProgetto.epicStoriesIds).toBeUndefined();
     });
 
-    test('should initialize with undefined name', () => {
-        expect(progetto.name).toBeUndefined();
-    });
+    test('should correctly construct a new project given the Id', async () => {
+        const expectedIds: string[] = ['1'];
 
-    test('should initialize with undefined isValidated', () => {
-        expect(progetto.isValidated).toBeUndefined();
+        expect(initializedProgetto.id).toBe('1');
+        expect(initializedProgetto.isValidated).toBe(true);
+        expect(initializedProgetto.name).toBe('Project A');
+        expect(initializedProgetto.epicStoriesIds).toEqual(expectedIds);
     });
-
-    test('should correctly fetch data for a new project', async () => {
-        await progetto.fetchData(new MockAPI());
-        expect(progetto.id).toBe('1');
-        expect(progetto.isValidated).toBe(true);
-        expect(progetto.name).toBe('Project A');
-    });
-
 }); 
