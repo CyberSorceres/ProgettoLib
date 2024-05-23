3d47418b57ab53b1460845e3031c6fedb926e4dc
uiob <uiob1210@gmail.com>
2024-05-23 15:08:40 +0200

    updated epicStory's tests

--- tests/unit/EpicStory.test.ts
+++ tests/unit/EpicStory.test.ts
@@ -3,58 +3,34 @@ import { EpicStory } from '../../src/EpicStory';
 import { MockAPI } from '../../src/MockAPI';
 import { mockEpicStories, mockUserStories } from '../../src/MockData';
 import { UserStory } from '../../src/UserStory';
+import { API_interface } from '../../src/API_interface';
+import { Progetto } from '../../src/Progetto';
 
 describe('EpicStory', () => {
 
-    let story: EpicStory;
+    let initializedEpic: EpicStory;
+    let nonInitializedEpic: EpicStory;
+    let api: API_interface;
 
-    beforeEach(() => {
-        story = new EpicStory('1');
+    beforeEach(async () => {
+        api = new MockAPI;
+        const epic = await api.getEpicStory('1');
+        if(epic != null){
+            initializedEpic = epic;
+        }
+        nonInitializedEpic = new EpicStory();
     });
 
-    test('should set the id correctly through the constructor and should return the correct id using the getter', () => {
-        expect(story.id).toBe('1');
+    test('non initialized EpicStory should have all fields undefined', () => {
+        expect(nonInitializedEpic.id).toBeUndefined;
+        expect(nonInitializedEpic.descrizione).toBeUndefined;
+        expect(nonInitializedEpic.userStoriesIds).toBeUndefined;
     })
 
-    test('should return undefined for descrizione if not set', () => {
-        expect(story.descrizione).toBeUndefined();
+    test('should correctly construct a new EpicStory', async () => {  
+        expect(initializedEpic.id).toBe('1');
+        expect(initializedEpic.descrizione).toBe('Epic Story 1');
+        expect(initializedEpic.userStoriesIds).toEqual(['1', '2', '3']);
     })
-
-    test('should set any descrizione correctly', () => {
-        const descrizione = 'This is a new Epic Story';
-        story.descrizione = descrizione;
-        expect(story.descrizione).toBe(descrizione);
-    })
-
-    test('should correctly fetch data for a new EpicStory', async () => {  
-        await story.fetchData(new MockAPI());
-        expect(story.id).toBe('1');
-        expect(story.descrizione).toBe("Epic Story 1"); 
-        expect(story.userStories[0]).toEqual(new UserStory('0', mockUserStories[0]));
-    })
-
-    test('should return the correct UserStory by ID', async () => {
-        await story.fetchData(new MockAPI());
-        const expectedUserStory = story.userStories[0];
-        const userStoryById = story.getUserStoryById(expectedUserStory.id);
-        expect(userStoryById).toEqual(expectedUserStory);
-    })
-    
-    test('should return undefined if UserStory ID is not found', () => {
-        const userStoryById = story.getUserStoryById('nonexistent_id');
-        expect(userStoryById).toBeUndefined();
-    })
-
-    test('should return the correct UserStory by tag', async () => {
-        await story.fetchData(new MockAPI());
-        const expectedUserStory = story.userStories[0];
-        const userStoryByTag = story.getUserStoryByTag(expectedUserStory.tag);
-        expect(userStoryByTag).toEqual(expectedUserStory);
-    });
-    
-    test('should return undefined if UserStory tag is not found', () => {
-        const userStoryByTag = story.getUserStoryByTag('nonexistent_tag');
-        expect(userStoryByTag).toBeUndefined();
-    });
     
 });
