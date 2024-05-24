3cb6bb3df031b46ae7535de73a5c66cd52150cb0
uiob <uiob1210@gmail.com>
2024-05-24 15:41:49 +0200

    added import and export for enum AI

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Progetto } from "./src/Progetto";
+import { Progetto, AI } from "./src/Progetto";
 import { EpicStory} from "./src/EpicStory";
 import { UserStory } from "./src/UserStory";
 import { Test } from "./src/Test";
@@ -9,4 +9,4 @@ import { exampleProjects, exampleEpicStories, exampleUserStories} from "./src/Mo
 import { RUOLO, User } from "./src/User";
 import { Feedback } from "./src/Feedback";
 
-export {Progetto, EpicStory, UserStory, Test, User, RUOLO, Feedback, API, API_interface, MockAPI, exampleProjects, exampleEpicStories, exampleUserStories};
+export {Progetto, EpicStory, UserStory, Test, User, RUOLO, Feedback, API, API_interface, MockAPI, exampleProjects, exampleEpicStories, exampleUserStories, AI};
