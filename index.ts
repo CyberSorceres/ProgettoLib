73f32641b223cb52530b52c385abd05fa1c5edd4
uiob <uiob1210@gmail.com>
2024-05-28 17:06:54 +0200

    add export

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { Progetto, AI } from "./src/Progetto";
 import { EpicStory} from "./src/EpicStory";
-import { UserStory } from "./src/UserStory";
+import { UserStory, State } from "./src/UserStory";
 import { Test } from "./src/Test";
 import { API } from "./src/API";
 import { API_interface } from "./src/API_interface";
@@ -9,4 +9,4 @@ import { exampleProjects, exampleEpicStories, exampleUserStories} from "./src/Mo
 import { RUOLO, User } from "./src/User";
 import { Feedback } from "./src/Feedback";
 
-export {Progetto, EpicStory, UserStory, Test, User, RUOLO, Feedback, API, API_interface, MockAPI, exampleProjects, exampleEpicStories, exampleUserStories, AI};
+export {Progetto, EpicStory, UserStory, State, Test, User, RUOLO, Feedback, API, API_interface, MockAPI, exampleProjects, exampleEpicStories, exampleUserStories, AI};
